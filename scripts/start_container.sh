e70d282782f8748d36632adddd4916871043b7e7
ephson24 <116164387+ephson24@users.noreply.github.com>
2024-08-28 13:58:58 +0200

    trigger

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,7 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull ephsong/simple-python-flask
 
-echo "Will run the container next"
+echo "Will run the container next 1"
 
 # Run the Docker image as a container
 docker run -d -p 8000:5000 ephsong/simple-python-flask
