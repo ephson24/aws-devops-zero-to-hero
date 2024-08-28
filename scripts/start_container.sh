dd58c974468f0017234fa68066321882d35acc25
ephson24 <116164387+ephson24@users.noreply.github.com>
2024-08-28 13:49:02 +0200

    Trigger codepipeline

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull ephsong/simple-python-flask
 
+echo "Will run the container next"
+
 # Run the Docker image as a container
 docker run -d -p 8000:5000 ephsong/simple-python-flask
