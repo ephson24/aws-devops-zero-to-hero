2aee8b91181176adadaea5947ecc1663fa37ea2e
ephson24 <116164387+ephson24@users.noreply.github.com>
2024-08-28 13:33:58 +0200

    mapped to my docker hub

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull ephsong/simple-python-flask
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 8000:5000 ephsong/simple-python-flask
